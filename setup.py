976716093a97292239bae7b700c7df5eedb59293
Stephen Ling <lingfromsh@163.com>
2022-02-19 09:46:18 +0800

    Fix metadata.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name="dj_loguru",
-    version="0.1.0",
+    version="0.1.1",
     description="Use loguru in Django.",
     author="Stephen Ling",
-    author_email="shiyun.ling@flexiv.com",
+    author_email="lingfromsh@163.com",
+    url="https://github.com/lingfromSh/django-loguru",
     keywords=["Django", "loguru"],
     license="MIT",
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=["loguru>=0.5.3", "Django>=3.2"],
     classifiers=[
-        "Development Status :: 1 - Alpha",
-        "Intended Audience :: Django Web Developers",
-        "Topic :: Software Development :: Logging",
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
